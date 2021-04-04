bfb8e8489c217dea8099067735d761d7f5ec36b1
Daniel <daniel@dangallaghermarketing.co.uk>
2021-04-04 13:44:58 +0100

    Added _redirects file, so going to /wp-admin redirects to wp.dangallaghermarketing which points to my wordpress site

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -32,6 +32,7 @@ const Form = (props) => {
       .then(() => {
         setLoading(false);
         setSubmitStatus("success");
+        reset();
       })
       .catch((err) => setSubmitStatus("fail"));
   };
